4c1e874c56c6c085df8a5befbbb89655ba10cf8e
jancarlohendriks <jancarlohendriks@hotmail.com>
2021-12-14 00:59:27 +0100

    intro image

--- src/sections/Intro.jsx
+++ src/sections/Intro.jsx
@@ -1,6 +1,6 @@
 import Link from "next/link";
 import Image from "next/image";
-import introPic from '../../public/images/intro.gif'
+// import introPic from '../../public/images/intro.gif'
 
 const Intro = () => {
 	return (
