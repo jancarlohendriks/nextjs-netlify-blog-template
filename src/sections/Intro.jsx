6a1d9113d71919a8f72817e4c6df3c972aab6080
jancarlohendriks <jancarlohendriks@hotmail.com>
2021-12-14 10:38:12 +0100

    restore

--- src/sections/Intro.jsx
+++ src/sections/Intro.jsx
@@ -1,5 +1,5 @@
 import Link from "next/link";
-import Image from "next/image";
+// import Image from "next/image";
 
 const Intro = () => {
 	return (
@@ -22,12 +22,12 @@ const Intro = () => {
 					</Link>
 				</div>
 				<div className="[ intro__media ] [ radius dot-shadow ]">
-					<Image
+					{/* <Image
 						className="[ intro__image ] [ radius ]"
 						src="/images/giphy.webp"
 						alt="Buttered toasted white bread"
 						layout="fill"
-					/>
+					/> */}
 					{/* <Image className="[ intro__image ] [ radius ]" src="/images/bg/toast.jpg" alt="Buttered toasted white bread" layout="fill" /> */}
 				</div>
 			</article>
